4cee805d9657d3b82d7540bd51a4c929118761af
Gabe Lynch <gabe3lynch@gmail.com>
2025-06-08 13:12:25 -0400

    Changed to lucide icons

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -1,31 +1,13 @@
-import { Minimize, Close, DragIndicator } from '@mui/icons-material';
+import { MinusIcon, XIcon, GripVerticalIcon } from 'lucide-react';
 
 import styles from "./ControlPanel.module.css";
-
-const CloseIcon = () => (
-  <div style={{ color: 'white' }}>
-    <Close fontSize="small" />
-  </div>
-);
-
-const MinimizeIcon = () => (
-  <div style={{ color: 'white' }}>
-    <Minimize fontSize="small" />
-  </div>
-);
-
-const DragIcon = () => (
-  <div style={{ cursor: 'grab', color: 'white' }}>
-    <DragIndicator fontSize="small" />
-  </div>
-);
 
 export default function ControlPanel() {
   return (
     <div className={styles.playIcons}>
-      <CloseIcon />
-      <DragIcon />
-      <MinimizeIcon />
+      <XIcon color="#ffffff"/>
+      <GripVerticalIcon color="#ffffff"/>
+      <MinusIcon color="#ffffff"/>
     </div>
   );
 }
--- src/components/PlaybackMenu/PlaybackMenu.tsx
+++ src/components/PlaybackMenu/PlaybackMenu.tsx
@@ -1,31 +1,13 @@
 import styles from "./PlaybackMenu.module.css";
 
-import { SkipPrevious, Pause, SkipNext } from "@mui/icons-material";
-
-const PreviousIcon = () => (
-  <div style={{ color: 'white' }}>
-    <SkipPrevious fontSize="small" />
-  </div>
-);
-
-const PauseIcon = () => (
-  <div style={{ color: 'white' }}>
-    <Pause fontSize="small" />
-  </div>
-);
-
-const NextIcon = () => (
-  <div style={{ cursor: 'grab', color: 'white' }}>
-    <SkipNext fontSize="small" />
-  </div>
-);
+import { SkipBackIcon, PauseIcon, SkipForwardIcon } from "lucide-react";
 
 export default function PlaybackMenu() {
   return (
     <div className={styles.playIcons}>
-      <PreviousIcon />
-      <PauseIcon />
-      <NextIcon />
+      <SkipBackIcon color="#ffffff"/>
+      <PauseIcon color="#ffffff"/>
+      <SkipForwardIcon color="#ffffff"/>
     </div>
   );
 }
